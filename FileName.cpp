56fd0990cebc5517d79d02660ddd24ce22490fe5
KrapX_me98 <KrapX_me98@28-9>
2024-09-04 11:29:11 +0300

    added a class

--- FileName.cpp
+++ FileName.cpp
@@ -1,1 +1,31 @@
 #include<iostream>
+using namespace std;
+
+class Point
+{
+private:
+	int x; 
+	int y;
+public:
+	void Print()
+	{
+		cout << "X: " << x << "\tY: " << y<<endl;
+	}
+	void Init(int a, int b)
+	{
+		x = a;
+		y = b;
+	}
+};
+
+int main()
+{
+	Point a;
+	a.Init(1, 2);
+	a.Print();
+
+	Point b;
+	b.Init(10, 20);
+	b.Print();
+
+}
